7696e2eddb7f438489c84c47ca24af72aa861972
delinuxist <delinuxist@gmail.com>
2022-05-03 09:47:04 +0000

    removed unused imports

--- src/components/utils/ScrollToTop.jsx
+++ src/components/utils/ScrollToTop.jsx
@@ -1,6 +1,5 @@
 import { useEffect,useState } from "react";
-import { BiArrowFromBottom, BiArrowToTop} from "react-icons/bi"
-import smooth from "react-scroll/modules/mixins/smooth";
+import { BiArrowFromBottom} from "react-icons/bi"
 import { classNames } from "./className";
 
 
@@ -32,11 +31,11 @@ export const ScrollToTop = () => {
   },[])
 
   return (
-    <div className=" fixed bottom-2 right-2">
+    <div className="fixed  bottom-2 right-2">
       <button onClick={scrollToTop} className={classNames(
         isVisible? " opacity-100": "opacity-0","inline-flex items-center p-3 rounded-full shadow-sm text-red-700 bg-yellow-50 transition-opacity hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-offset-2 m-5 focus:ring-yellow-400 "
       )}>
-        <BiArrowFromBottom className=" h-7 w-6" aria-hidden="true"/>
+        <BiArrowFromBottom className="w-6  h-7" aria-hidden="true"/>
       </button>
     </div>
   )
