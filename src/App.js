a66b6a842877ad8b8ce50b6d760389bee31fb351
delinuxist <delinuxist@gmail.com>
2022-05-03 01:19:55 +0000

    all pages imported

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import About from "./components/About";
+import Platforms from "./components/Platforms";
+import Hero from "./components/Hero";
+import Footer from "./components/layout/Footer";
+import Navbar from "./components/layout/Navbar";
+import Pricing from "./components/Pricing";
+import Support from "./components/Support";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Support />
+      <Platforms />
+      <Pricing />
+      <Footer />
+    </>
   );
 }
 
