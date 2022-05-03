305efe0540290987fb9eff1563bf465cf04dd163
delinuxist <delinuxist@gmail.com>
2022-05-03 09:36:40 +0000

    removed imported scroll

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,5 +1,5 @@
 import React,{useState} from 'react'
-import { Link, animateScroll as scroll } from 'react-scroll'
+import { Link } from 'react-scroll'
 
 import {MenuIcon,XIcon} from "@heroicons/react/outline"
 
@@ -11,13 +11,13 @@ const Navbar = () => {
 
   return (
     <div  className='w-screen h-[80px] z-10 fixed bg-yellow-50 drop-shadow-lg '>
-      <div className=' px-2 flex justify-between items-center w-full h-full'>
-        <div className=' flex items-center' >
-          <div className=' flex flex-col items-center'>
-          <h1 className=' text-3xl font-bold mr-4 sm:text-4xl'>Cyber<span className=' text-gray-600'>Corp</span></h1>
-          <h2 className=' text-xs'>Security</h2>
+      <div className='flex items-center justify-between w-full h-full px-2 '>
+        <div className='flex items-center ' >
+          <div className='flex flex-col items-center '>
+          <h1 className='mr-4 text-3xl font-bold  sm:text-4xl'>Cyber<span className='text-gray-600 '>Corp</span></h1>
+          <h2 className='text-xs '>Security</h2>
           </div>
-          <ul className=' hidden md:flex'>
+          <ul className='hidden  md:flex'>
             <li>
             <Link  to="home"  smooth={true}  duration={-200} >
           Home
@@ -44,50 +44,50 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
-        <div className=' hidden md:flex pr-4'>
-          <button  className=' border-none bg-transparent text-black mr-4'>
+        <div className='hidden pr-4  md:flex'>
+          <button  className='mr-4 text-black bg-transparent border-none '>
             Sign In
           </button>
-          <button className=' px-8 py-3'>
+          <button className='px-8 py-3 '>
             Sign Up
           </button>
         </div>
         <div onClick={handleClick} className='md:hidden '>
-        {nav ?<XIcon className='w-5 mr-4'/>: <MenuIcon className=' w-5 mr-4'/> }
+        {nav ?<XIcon className='w-5 mr-4'/>: <MenuIcon className='w-5 mr-4 '/> }
         </div>
       </div>
 
       <ul className={nav ? ' absolute bg-zinc-200 w-full px-8 text-center':"hidden"}>
-      <li className=' border-b-2 border-zinc-300 w-full'>
+      <li className='w-full border-b-2  border-zinc-300'>
             <Link onClick={handleClose}  to="home" spy={true} smooth={true}  duration={-200} >
           Home
         </Link>
             </li>
-            <li className=' border-b-2 border-zinc-300 w-full'>
+            <li className='w-full border-b-2  border-zinc-300'>
             <Link onClick={handleClose}  to="about" spy={true} smooth={true} offset={-200} duration={500} >
           About
         </Link>
             </li>
-            <li className=' border-b-2 border-zinc-300 w-full'>
+            <li className='w-full border-b-2  border-zinc-300'>
             <Link onClick={handleClose}  to="support" spy={true} smooth={true} offset={-50} duration={500} >
           Support
         </Link>
             </li>
-            <li className=' border-b-2 border-zinc-300 w-full'>
+            <li className='w-full border-b-2  border-zinc-300'>
             <Link onClick={handleClose}  to="platforms" spy={true} smooth={true} offset={-100} duration={500} >
           Platforms
         </Link> </li>
-            <li className=' border-b-2 border-zinc-300 w-full'>
+            <li className='w-full border-b-2  border-zinc-300'>
             <Link onClick={handleClose}  to="pricing" spy={true} smooth={true} offset={-50} duration={500} >
           Pricing
         </Link>
             </li>
            
             <div className='flex flex-col my-4'>
-            <button  className=' bg-transparent text-indigo-600 px-8 py-3 mb-4'>
+            <button  className='px-8 py-3 mb-4 text-indigo-600 bg-transparent '>
             Sign In
           </button>
-          <button className=' px-8 py-3  '>
+          <button className='px-8 py-3 '>
             Sign Up
           </button>
             </div>
